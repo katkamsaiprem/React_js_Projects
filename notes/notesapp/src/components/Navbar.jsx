e88584902987217781cb530f585f22a60458c0fd
Saiprem <katukamsaiprem@gmail.com>
2025-08-11 11:57:50 +0530

    structureDone

---
+++ notes/notesapp/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from '../assets/logo.png';
+export const Navbar = () => {
+
+    
+  return (
+    <>
+      <header>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <h1>Note</h1>
+      </header>
+    </>
+  );
+};
